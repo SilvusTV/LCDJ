d0926b486d25f6b388b55fffcc6289cb7d85bb63
SilvusTV <hugo.combe.84@gmail.com>
2024-08-18 14:27:55 +0200

    feat : increase image size on phone

--- inertia/components/Home/News.tsx
+++ inertia/components/Home/News.tsx
@@ -2,12 +2,12 @@ export default function News() {
   return (
     <div className={'h-screen bg-orange-dark flex flex-col items-center'}>
       <h1 className={'text-6xl text-center font-extrabold m-20 max-sm:text-5xl'}>Nos Actus</h1>
-      <div className={'grid grid-cols-2 grid-rows-3 gap-10 h-3/4 w-4/5'}>
-        <div
-          className={
-            'row-span-2 relative overflow-hidden flex items-end max-sm:col-span-2 max-sm:row-span-2'
-          }
-        >
+      <div
+        className={
+          'grid sm:grid-cols-2 sm:grid-rows-2 sm:mb-52 gap-10 h-3/4 w-4/5 max-sm:grid-cols-1 max-sm:grid-rows-3'
+        }
+      >
+        <div className={'sm:row-span-2 relative overflow-hidden flex items-end'}>
           <a
             className={
               'hover:tracking-widest hover-animation z-1 w-full h-full content-end bg-gradient-to-b from-transparent to-black from-40%'
@@ -23,7 +23,7 @@ export default function News() {
             className={'absolute top-0 left-0 w-full h-full object-cover'}
           />
         </div>
-        <div className={'relative overflow-hidden max-sm:row-start-3 flex items-end'}>
+        <div className={'relative overflow-hidden max-sm:row-start-2 flex items-end'}>
           <a
             className={
               'hover:tracking-widest hover-animation z-1 w-full h-full content-end bg-gradient-to-b from-transparent to-black from-40%'
@@ -41,7 +41,9 @@ export default function News() {
             className={'absolute top-0 left-0 w-full h-full object-cover'}
           />
         </div>
-        <div className={'relative overflow-hidden flex items-end col-start-2 max-sm:row-start-3'}>
+        <div
+          className={'relative overflow-hidden flex items-end sm:col-start-2 max-sm:row-start-3'}
+        >
           <a
             className={
               'hover:tracking-widest hover-animation z-1 w-full h-full content-end bg-gradient-to-b from-transparent to-black from-40%'
